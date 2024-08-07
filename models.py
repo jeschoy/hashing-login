0e6f281778045364759e56996cc8fca2f38f8f91
Jessica Choi <jesymchoi@gmail.com>
2024-08-07 14:44:57 -0700

    added user model

--- models.py
+++ models.py
@@ -9,3 +9,14 @@ def connect_db(app):
 
   db.app = app
   db.init_app(app)
+
+class User(db.Model):
+  """User model to store user information"""
+  __tablename__ = 'users'
+
+  username = db.Column(db.String(20), primary_key=True, unique=True, nullable=False)
+  password = db.Column(db.Text, nullable=False)
+  email = db.Column(db.String(50), nullable=False)
+  first_name = db.Column(db.String(30), nullable=False)
+  last_name = db.Column(db.String(30), nullable=False)
+  
